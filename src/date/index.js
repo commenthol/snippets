39b5d369ffd2c442ba3796c8bacd54a258fc70b2
commenthol <commenthol@gmail.com>
2019-02-10 22:22:02 +0100

    feat: Interval

--- src/date/index.js
+++ src/date/index.js
@@ -1,1 +1,2 @@
 export * from './dayOfYear'
+export * from './Interval'
---
+++ test/date/Interval.js
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import { Interval } from '../../src/date'
+
+describe('date/Interval', () => {
+  it('should run', function (done) {
+    let n = 0
+    const start = Date.now()
+    const fn = () => {
+      if (++n >= 4) {
+        interval.clear()
+        assert.ok(Date.now() - start >= 40)
+        done()
+      }
+    }
+    const interval = new Interval(fn, 10)
+  })
+})
