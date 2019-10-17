6bd02b6753643c70037601e1680d2288bc6ce2e9
commenthol <commenthol@gmail.com>
2019-10-17 07:12:54 +0200

    fix(merge): replace hasOwnPropery

--- src/object/merge.js
+++ src/object/merge.js
@@ -7,8 +7,8 @@
 export const merge = (...objs) =>
   [...objs].reduce(
     (acc, obj) => Object.keys(obj).reduce((_a, k) => {
-      if (k !== '__proto__') {
-        acc[k] = acc.hasOwnProperty(k) && typeof acc[k] === 'object'
+      if (k !== '__proto__') { // avoid prototype pollution
+        acc[k] = Object.prototype.hasOwnProperty.call(acc, k) && typeof acc[k] === 'object'
           ? merge(acc[k], obj[k])
           : obj[k]
       }
