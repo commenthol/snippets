ca6214e0b04a11fff2db4a6780d4b966e9613135
commenthol <commenthol@gmail.com>
2019-11-18 23:14:34 +0100

    feat(downloadWithRedirects): add decompression

--- src/node/downloadWithRedirects.js
+++ src/node/downloadWithRedirects.js
@@ -4,11 +4,20 @@ import fs from 'fs'
 import http from 'http'
 import https from 'https'
 import { parse } from 'url'
+import { Transform } from 'stream'
+import zlib from 'zlib'
+
+class Through extends Transform {
+  _transform (chunk, enc, done) {
+    this.push(chunk, enc)
+    done()
+  }
+}
 
 /**
  * download url to file - follows redirects
  * @param {String} url
- * @param {Path} filename
+ * @param {String} filename
  * @returns {Promise}
  */
 export const download = (url, filename) => {
@@ -17,17 +26,30 @@ export const download = (url, filename) => {
   const getUrl = (url) => ({
     ...parse(url),
     headers: {
-      'User-Agent': 'Mozilla/5.0',
-      Accept: '*/*'
+      'User-Agent': 'node/12.0',
+      Accept: '*/*',
+      'Accept-Encoding': 'gzip, deflate, br'
     }
   })
 
   return new Promise((resolve, reject) => {
-    const error = (err) => reject(err)
-    file.on('error', error)
+    const handleError = (err) => reject(err)
+    file.on('error', handleError)
     file.on('finish', () => resolve())
 
-    function makeReq (url, depth) {
+    const decompressor = (contentEncoding) => {
+      const stream = contentEncoding === 'br'
+        ? zlib.createBrotliDecompress()
+        : contentEncoding === 'gzip'
+          ? zlib.createGunzip()
+          : contentEncoding === 'deflate'
+            ? zlib.createDeflate()
+            : new Through()
+      stream.on('error', handleError)
+      return stream
+    }
+
+    const makeReq = (url, depth) => {
       const prot = url.indexOf('https') === 0 ? https : http
       const req = prot.get(getUrl(url), res => {
         const code = String(res.statusCode)[0]
@@ -36,16 +58,17 @@ export const download = (url, filename) => {
           return
         }
         if (code === '2') {
-          res.pipe(file).on('error', error)
-          res.on('error', error)
+          const contentEncoding = res.headers['content-encoding']
+          res.pipe(decompressor(contentEncoding)).pipe(file)
+          res.on('error', handleError)
         } else {
-          reject(new Error(res.statusCode))
+          reject(new Error(String(res.statusCode)))
         }
       })
-      req.on('error', error)
+      req.on('error', handleError)
       req.end()
     }
 
-    makeReq(url, 5)
+    makeReq(url, 7)
   })
 }
