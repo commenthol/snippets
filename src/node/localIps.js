880f9d704bcbcab9abb3b207fac55fb89f2df083
commenthol <commenthol@gmail.com>
2019-11-18 22:15:40 +0100

    feat: obtain local ip addresses

---
+++ src/node/localIps.js
@@ -0,0 +1,20 @@
+
+import os from 'os'
+
+const flatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? flatten(v) : v)))
+
+/**
+ * return list of local ip addresses
+ * @return {Object[]} array of objects `{name, address}`
+ */
+export const localIps = () => flatten(
+  Object.entries(os.networkInterfaces()).map(([name, addresses]) =>
+    addresses.map(({ family, internal, address }) => {
+      if (family === 'IPv4' && internal !== true) {
+        return { name, address }
+      }
+    }).filter(Boolean)
+  ).filter(Boolean)
+)
+
+console.log(localIps()) // eslint-disable-line
