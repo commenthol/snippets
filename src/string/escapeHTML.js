c09787bebd1688d56ddde97bacb49de443e6ac3b
commenthol <commenthol@gmail.com>
2019-11-18 22:14:53 +0100

    feat: escapeHtmlLiteral

--- src/string/escapeHTML.js
+++ src/string/escapeHTML.js
@@ -1,10 +1,12 @@
 /**
  * escape HTML and prevent double escaping of '&'
+ * @param {String} string - which requires escaping
+ * @returns {String} escaped string
  * @example
  * escapeHTML('<h1>"One" & 'Two' &amp; Works</h1>')
  * // &lt;h1&gt;&quot;One&quot; &amp; &#39;Two&#39; &amp; Works&lt;/h1&gt;
  */
-export const escapeHTML = string => string
+export const escapeHtml = string => (string || '')
   .replace(/&amp;/g, '&')
   .replace(/[&<>'"]/g, tag => ({
     '&': '&amp;',
@@ -13,3 +15,16 @@ export const escapeHTML = string => string
     "'": '&#39;',
     '"': '&quot;'
   }[tag]))
+
+/**
+ * Escape all vars in a template literal
+ * @param {*} literals
+ * @param  {...any} vars
+ * @returns {String}
+ * @example
+ * escapeHtmlLiteral('<h1>"One" & 'Two' &amp; Works</h1>')
+ * // &lt;h1&gt;&quot;One&quot; &amp; &#39;Two&#39; &amp; Works&lt;/h1&gt;
+ */
+export const escapeHtmlLiteral = (literals, ...vars) => literals
+  .map((literal, i) => literal + escapeHtml(vars[i]))
+  .join('')
--- test/string/escapeHTML.js
+++ test/string/escapeHTML.js
@@ -1,11 +1,18 @@
 import assert from 'assert'
-import { escapeHTML } from '../../src/string'
+import { escapeHtml, escapeHtmlLiteral } from '../../src/string'
 
 describe('string/escapeHTML', () => {
   it('should escape HTML', () => {
     assert.strictEqual(
-      escapeHTML('<h1><a href="/test?query=1&test=2">Does &amp; Works</a></h1>'),
+      escapeHtml('<h1><a href="/test?query=1&test=2">Does &amp; Works</a></h1>'),
       '&lt;h1&gt;&lt;a href=&quot;/test?query=1&amp;test=2&quot;&gt;Does &amp; Works&lt;/a&gt;&lt;/h1&gt;'
+    )
+  })
+
+  it('should escape HTML template', () => {
+    assert.strictEqual(
+      escapeHtmlLiteral`<h1><a href="${'/<test?query=1&test=2'}">${'<script>Does & Works >'}</a></h1>`,
+      '<h1><a href="/&lt;test?query=1&amp;test=2">&lt;script&gt;Does &amp; Works &gt;</a></h1>'
     )
   })
 })
