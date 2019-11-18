0451cd9447e72ae2e9a48bf2931809a21d72a852
commenthol <commenthol@gmail.com>
2019-11-18 23:15:49 +0100

    fix(downloadWithRedirects): export and test

--- test/node/download.js
+++ test/node/download.js
@@ -1,13 +1,24 @@
 import assert from 'assert'
 import os from 'os'
 import fs from 'fs'
-import { download } from '../..'// "/src/node"
+import { download, downloadWithRedirects } from '../..'// "/src/node"
 
 describe('node/download', () => {
   it('should download to file', () => {
     const target = `${os.tmpdir()}/nodejs-org.html`
 
     return download('https://nodejs.org/en/', target)
+      .then(() => {
+        const stat = fs.statSync(target)
+        assert.ok(stat.isFile())
+      })
+      .catch(err => assert.ok(!err, 'should download target'))
+  })
+
+  it('should download to file using redirects and compression', () => {
+    const target = `${os.tmpdir()}/nodejs-org.1.html`
+
+    return downloadWithRedirects('http://nodejs.org/en/', target)
       .then(() => {
         const stat = fs.statSync(target)
         assert.ok(stat.isFile())
