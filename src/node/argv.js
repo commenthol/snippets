f86317236c1bc117d75831b74506b38812353dcb
commenthol <commenthol@gmail.com>
2019-11-19 21:28:43 +0100

    feat(node/argv): argument parser

---
+++ src/node/argv.js
@@ -0,0 +1,31 @@
+
+const path = require('path')
+
+function argv (args) {
+  const argv = args || process.argv.slice(2)
+  const cmd = { files: [] }
+
+  while (argv.length) {
+    const arg = argv.shift()
+
+    switch (arg) {
+      case '-h':
+      case '--help':
+        cmd.help = true
+        break
+      case '-v':
+      case '--version':
+        cmd.version = true
+        break
+      case '-t':
+      case '--todo':
+        cmd.todo = argv.shift()
+        break
+      default:
+        cmd.files.push(path.resolve(process.cwd(), arg))
+    }
+  }
+  return cmd
+}
+
+module.exports = argv
---
+++ test/node/argv.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+const { resolve } = require('path')
+const argv = require('../../src/node/argv.js')
+
+describe('argv', function () {
+  it('shall parse arguments', function () {
+    const cmd = argv('--help --version --todo todo file1 file2'.split(/ /))
+    assert.deepStrictEqual(cmd, {
+      files: [
+        resolve(process.cwd(), 'file1'),
+        resolve(process.cwd(), 'file2')
+      ],
+      help: true,
+      todo: 'todo',
+      version: true
+    })
+  })
+  it('shall parse short arguments', function () {
+    const cmd = argv('-h file1 -v file2 -t todo'.split(/ /))
+    assert.deepStrictEqual(cmd, {
+      files: [
+        resolve(process.cwd(), 'file1'),
+        resolve(process.cwd(), 'file2')
+      ],
+      help: true,
+      todo: 'todo',
+      version: true
+    })
+  })
+})
