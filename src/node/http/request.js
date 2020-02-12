04ad807f4277dc4b10e084474c42c40382ea4462
commenthol <commenthol@gmail.com>
2020-02-12 20:27:01 +0100

    feat: request with timeout

--- src/node/http/request.js
+++ src/node/http/request.js
@@ -3,7 +3,7 @@
 // const { parse } = require('url') // eslint-disable-line node/no-deprecated-api
 import http from 'http'
 import https from 'https'
-import { parse } from 'url' // eslint-disable-line node/no-deprecated-api
+import { parse, format, resolve } from 'url' // eslint-disable-line node/no-deprecated-api
 
 /**
  * http(s) request with redirects
@@ -15,46 +15,84 @@ import { parse } from 'url' // eslint-disable-line node/no-deprecated-api
  */
 export function request (url, method = 'GET', opts) {
   let _data = ''
+  if (typeof method === 'object') {
+    opts = method
+    method = 'GET'
+  }
   method = method.toUpperCase()
-  opts = Object.assign({ method, redirects: 7 }, parse(url), opts)
+  opts = Object.assign({ method, redirects: 7, timeout: 30000 }, parse(url), opts)
   const locations = []
+
+  const redirect = (opts, location) => {
+    const loc = parse(location)
+    const { protocol, host, hostname, port, pathname } = opts
+
+    Object.assign(opts, loc)
+
+    if (!loc.hostname) {
+      Object.assign(opts, { protocol, host, hostname, port })
+      if (loc.pathname[0] === '.') {
+        opts.pathname = resolve(pathname, loc.pathname)
+        opts.path = opts.pathname + (loc.search || '')
+      }
+      opts.href = format(opts)
+    }
+
+    return opts
+  }
 
   const end = (cb) => {
     const protocol = opts.protocol === 'https:' ? https : http
     const req = protocol.request(opts)
+
+    const handleError = (err) => {
+      clearTimeout(timer)
+      cb(err)
+    }
+    const timer = opts.timeout && setTimeout(() => {
+      req.destroy(new Error('err_timeout'))
+    }, opts.timeout)
+
     req.on('response', res => {
       // @ts-ignore
       res.text = ''
       const { location } = res.headers
+      clearTimeout(timer)
       if ([301, 302].indexOf(res.statusCode) !== -1 && location && locations.length < opts.redirects) {
-        opts = Object.assign(opts, parse(location))
-        locations.push(location)
+        opts = redirect(opts, location)
+        locations.push(opts.href)
         end(cb)
       } else {
         // @ts-ignore
         if (locations.length) res.locations = locations
-        res.on('error', cb)
+        res.on('error', handleError)
         // @ts-ignore
         res.on('data', chunk => { res.text += chunk })
         res.on('end', () => cb(null, res))
       }
     })
-    req.on('error', cb)
+    req.on('error', handleError)
     req.end(_data)
   }
 
   const pipe = (stream) => {
     const protocol = opts.protocol === 'https:' ? https : http
     const req = protocol.request(opts)
+
     const handleError = (err) => {
+      clearTimeout(timer)
       stream.emit('error', err)
     }
+    const timer = opts.timeout && setTimeout(() => {
+      req.destroy(new Error('err_timeout'))
+    }, opts.timeout)
 
     req.on('response', res => {
       const { location } = res.headers
+      clearTimeout(timer)
       if ([301, 302].indexOf(res.statusCode) !== -1 && location && locations.length < opts.redirects) {
-        opts = Object.assign(opts, parse(location))
-        locations.push(location)
+        opts = redirect(opts, location)
+        locations.push(opts.href)
         pipe(stream)
       } else {
         // @ts-ignore
@@ -78,6 +116,10 @@ export function request (url, method = 'GET', opts) {
       _data += data
       return self
     },
+    timeout: (timeout) => {
+      opts.timeout = timeout
+      return self
+    },
     auth: (username, password) => {
       if (/:/.test(username)) throw new TypeError('username contains colons')
       const base64 = Buffer.from(`${username}:${password}`).toString('base64')
@@ -90,23 +132,23 @@ export function request (url, method = 'GET', opts) {
       return self
     },
     pipe: (stream) => pipe(stream),
-    end: (cb) => end(cb),
-    then: async (_resolve, _reject) => {
-      return new Promise((resolve, reject) => {
-        end((err, res) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(res)
-          }
+    end: (cb) => {
+      if (!cb) {
+        return new Promise((resolve, reject) => {
+          end((err, res) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(res)
+            }
+          })
         })
-      }).then(_resolve, _reject)
+      } else {
+        end(cb)
+      }
     },
-    catch: async (errFn) => {
-      return self
-        .then(res => res)
-        .catch(errFn)
-    }
+    then: async (resolveFn, rejectFn) => self.end().then(resolveFn, rejectFn),
+    catch: async (errFn) => self.then(res => res).catch(errFn)
   }
 
   http.METHODS.forEach(method => {
--- test/node/http/request.js
+++ test/node/http/request.js
@@ -1,24 +1,70 @@
 import assert from 'assert'
+import http from 'http'
 import { Writable } from 'stream'
 import request from '../../../src/node/http/request'
 
 describe('node/http/request', function () {
   this.timeout(8000)
+  let server
+
+  const port = 30003
+  const host = `http://localhost:${port}`
+
+  before((done) => {
+    server = http.createServer((req, res) => {
+      const { method, url } = req
+      // console.log(method, url)
+      if (url === '/head') {
+        if (method !== 'HEAD') {
+          res.statusCode = 500
+        }
+        res.end()
+      } else if (url === '/godot') {
+        // never turn back
+      } else if (url === '/redirect') {
+        res.setHeader('location', '../../redirect1?foo=bar#hash')
+        res.statusCode = 302
+        res.end()
+      } else if (url.indexOf('/redirect1') === 0) {
+        res.setHeader('location', '/')
+        res.statusCode = 302
+        res.end()
+      } else {
+        res.end('<!DOCTYPE html>\n<html><body>works</body></html>')
+      }
+    })
+    server.listen(port, done)
+  })
+
+  after(() => {
+    server.close()
+  })
+
+  const headers = { 'User-Agent': 'request/1.0' }
 
   it('shall request and follow redirects', function (done) {
-    request('http://duck.com')
+    request(host + '/redirect')
+      .set(headers)
       .end((err, res) => {
         // err && console.log(err)
         // res && console.log(res.text, res.headers, res.locations)
         assert.ok(!err, err && err.message)
         assert.ok(/DOCTYPE html/.test(res.text))
-        assert.deepStrictEqual(res.locations, ['https://duck.com/', 'https://duckduckgo.com'])
+        assert.deepStrictEqual(res.locations, [host + '/redirect1?foo=bar#hash', host + '/'])
         done()
       })
   })
 
+  it('shall use HEAD request', async function () {
+    const res = await request(host + '/head')
+      .set(headers)
+      .head()
+      .end()
+    assert.strictEqual(res.statusCode, 200)
+  })
+
   it('shall return a promise', function () {
-    return request('https://duckduckgo.com')
+    return request(host)
       .then((res) => {
         // res && console.log(res.text, res.headers, res.locations)
         assert.ok(/DOCTYPE html/.test(res.text))
@@ -38,12 +84,40 @@ describe('node/http/request', function () {
       done()
     })
     writer.on('response', (res) => {
-      assert.deepStrictEqual(res.locations, ['https://duckduckgo.com'])
+      assert.deepStrictEqual(res.locations, [host + '/redirect1?foo=bar#hash', host + '/'])
     })
     writer.on('error', (err) => {
       assert.ok(false, err) // never reach here
     })
-    request('https://duck.com')
+    request(host + '/redirect')
       .pipe(writer)
+  })
+
+  describe('timeout', function () {
+    it('shall return a timeout error', function () {
+      return request(host + '/godot')
+        .timeout(1000)
+        .then(() => {
+          assert.ok(true, 'shall never reach here')
+        })
+        .catch(err => {
+          assert.strictEqual(err.message, 'err_timeout')
+        })
+    })
+
+    it('shall return a timeout error if streaming', function (done) {
+      const write = (chunk, enc, done) => {
+        done()
+      }
+      const writer = new Writable({ write })
+      writer.on('error', (err) => {
+        assert.strictEqual(err.message, 'err_timeout')
+        done()
+      })
+
+      request(host + '/godot')
+        .timeout(1000)
+        .pipe(writer)
+    })
   })
 })
