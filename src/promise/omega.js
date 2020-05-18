1eb975d21710a50ed71519fcf5f152853d206120
commenthol <commenthol@gmail.com>
2020-05-18 20:19:41 +0200

    break: change omega api

--- src/promise/omega.js
+++ src/promise/omega.js
@@ -1,8 +1,12 @@
 /**
  * replace try/catch boilerplaite with go like return values
  * @see https://gobyexample.com/errors
+ * @example
+ * const fn = async (x) => Promise.resolve(x + 4)
+ * const [err, result] = await omega(fn)(6)
+ * //> err == null, result == 10
  */
-export const omega = async (fn, ...args) => {
+export const omega = fn => async (...args) => {
   try {
     const result = await fn(...args)
     return [null, result]
--- test/promise/omega.js
+++ test/promise/omega.js
@@ -4,25 +4,25 @@ import { omega } from '../..'
 describe('promise/omega', () => {
   it('shall return result', async () => {
     const fn = async (x) => Promise.resolve(x + 4)
-    const [err, result] = await omega(fn, 6)
+    const [err, result] = await omega(fn)(6)
     assert.strictEqual(err, null)
     assert.strictEqual(result, 10)
   })
   it('shall return error', async () => {
     const fn = async (x) => Promise.reject(new TypeError('bamm'))
-    const [err, result] = await omega(fn, 6)
+    const [err, result] = await omega(fn)(6)
     assert.strictEqual(err.message, 'bamm')
     assert.strictEqual(result, undefined)
   })
   it('shall return result - sync fn', async () => {
     const fn = async (x) => (x + 4)
-    const [err, result] = await omega(fn, 6)
+    const [err, result] = await omega(fn)(6)
     assert.strictEqual(err, null)
     assert.strictEqual(result, 10)
   })
   it('shall return error - sync fn', async () => {
     const fn = async (x) => { throw new TypeError('bamm') }
-    const [err, result] = await omega(fn, 6)
+    const [err, result] = await omega(fn)(6)
     assert.strictEqual(err.message, 'bamm')
     assert.strictEqual(result, undefined)
   })
