cfa9eb9ffad03077f8e919396b1f4e19e27eb5fd
commenthol <commenthol@gmail.com>
2020-10-04 10:05:38 +0200

    feat(string/template): template literals on strings

---
+++ src/string/template.js
@@ -0,0 +1,46 @@
+const PLACEHOLDER = /\${\s*([^}]+)\s*}/
+
+/**
+ * template function to use template literals on strings e.g. from JSON data
+ * only placeholder replacement from object props is available.
+ *
+ * @param {string} [str=''] - template string
+ * @param {RegExp} [placeholder=PLACEHOLDER] - regular expression for placeholder
+ * @return {function} compiled template
+ * @example
+ * const compiled = template(';; ${firstname} - ${lastname} ;;')
+ * const r = compiled({ firstname: 'Alice', lastname: 'Anders' })
+ * > r = ';; Alice - Anders ;;'
+ */
+export const template = (str = '', placeholder = PLACEHOLDER) => {
+  const prop = {}
+  const tmpl = []
+
+  while (str.length) {
+    const m = placeholder.exec(str)
+    if (m) {
+      const pre = str.substring(0, m.index)
+      str = str.substr(m.index + m[0].length)
+      tmpl.push(pre)
+      prop[m[1]] = tmpl.length
+      tmpl.push('')
+    } else {
+      tmpl.push(str)
+      str = ''
+    }
+  }
+
+  return (obj) => {
+    let found = false
+    if (!obj) return ''
+    const str = Object.entries(obj).reduce((a, [key, val]) => {
+      const pos = prop[key] || -1
+      if (pos > -1) {
+        found = true
+        a[pos] = val
+      }
+      return a
+    }, [...tmpl]).join('').trim()
+    return found ? str : ''
+  }
+}
---
+++ src/string/template.spec.js
@@ -0,0 +1,33 @@
+/* eslint no-template-curly-in-string: off */
+import assert from 'assert'
+import { template } from './template.js'
+
+describe('ui/utils/template', () => {
+  it('shall compile template', () => {
+    const t = template(';; ${firstname} - ${lastname} ;;')
+    const r = t({ firstname: 'Alice', lastname: 'Anders' })
+    assert.strictEqual(r, ';; Alice - Anders ;;')
+  })
+
+  it('shall compile template with missing prop', () => {
+    const t = template(';; ${firstname} - ${ lastname  } ;;')
+    const r = t({ firstname: 'Alice' })
+    assert.strictEqual(r, ';; Alice -  ;;')
+  })
+
+  it('shall return empty string on missing obj', () => {
+    const t = template(';; ${firstname} - ${ lastname  } ;;')
+    const r = t()
+    assert.strictEqual(r, '')
+  })
+
+  it('shall return empty string if no templated prop is found', () => {
+    const t = template(';; ${firstname} - ${ lastname  } ;;')
+    const r = t({ ok: true })
+    assert.strictEqual(r, '')
+  })
+
+  it('shall not throw on missing template', () => {
+    template()()
+  })
+})
