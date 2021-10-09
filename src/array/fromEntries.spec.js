66364e6763e09956ba6bbf3a319a742df0c55513
commenthol <commenthol@gmail.com>
2021-10-09 10:55:32 +0200

    fix(array): fromEntries linter issue

--- src/array/fromEntries.spec.js
+++ src/array/fromEntries.spec.js
@@ -3,7 +3,7 @@ import { fromEntries, fromEntriesNative } from './index.js'
 
 describe('array/fromEntries', () => {
   const arr = [['a', 1], ['b', 2], ['c', 3]]
-  const exp = { a: 1, b: 2, c: 3}
+  const exp = { a: 1, b: 2, c: 3 }
 
   it('should build object ES2021', () => {
     assert.deepStrictEqual(fromEntries(arr), exp)
