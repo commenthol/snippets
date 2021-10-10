3e091713a6749ab60c827bfdc44c1046fbf24b74
commenthol <commenthol@gmail.com>
2021-10-10 20:29:36 +0200

    feat: terminal ansi colors

---
+++ src/node/colors.js
@@ -0,0 +1,55 @@
+/**
+ * ansi colors
+ */
+
+const esc = (s) => '\u001b[' + (isNaN(s) ? s : s + 'm')
+
+// modifier
+export const resetAll = esc`0`
+export const bold = esc`1`
+export const dim = esc`2`
+export const italic = esc`3`
+export const underline = esc`4`
+export const inverse = esc`7`
+export const hidden = esc`8`
+export const strikethrough = esc`9`
+
+// colors
+export const black = esc`30`
+export const red = esc`31`
+export const green = esc`32`
+export const yellow = esc`33`
+export const blue = esc`34`
+export const magenta = esc`35`
+export const cyan = esc`36`
+export const white = esc`37`
+export const gray = esc`90`
+export const blackBright = esc`90`
+export const redBright = esc`91`
+export const greenBright = esc`92`
+export const yellowBright = esc`93`
+export const blueBright = esc`94`
+export const magentaBright = esc`95`
+export const cyanBright = esc`96`
+export const whiteBright = esc`97`
+export const reset = esc`39`
+
+// background colors
+export const bgBlack = esc`40`
+export const bgRed = esc`41`
+export const bgGreen = esc`42`
+export const bgYellow = esc`43`
+export const bgBlue = esc`44`
+export const bgMagenta = esc`45`
+export const bgCyan = esc`46`
+export const bgWhite = esc`47`
+export const bgBlackBright = esc`100`
+export const bgRedBright = esc`101`
+export const bgGreenBright = esc`102`
+export const bgYellowBright = esc`103`
+export const bgBlueBright = esc`104`
+export const bgMagentaBright = esc`105`
+export const bgCyanBright = esc`106`
+export const bgWhiteBright = esc`107`
+export const bgReset = esc`49`
+export const bgExpand = esc`K`
---
+++ src/node/colors.spec.js
@@ -0,0 +1,107 @@
+import assert from 'assert'
+import {
+  resetAll,
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  gray,
+  blackBright,
+  redBright,
+  greenBright,
+  yellowBright,
+  blueBright,
+  magentaBright,
+  cyanBright,
+  whiteBright,
+  reset,
+  bgBlack,
+  bgRed,
+  bgGreen,
+  bgYellow,
+  bgBlue,
+  bgMagenta,
+  bgCyan,
+  bgWhite,
+  bgBlackBright,
+  bgRedBright,
+  bgGreenBright,
+  bgYellowBright,
+  bgBlueBright,
+  bgMagentaBright,
+  bgCyanBright,
+  bgWhiteBright,
+  bgReset,
+  bgExpand
+} from './colors.js'
+
+describe('colors', function () {
+  it('shall display colors', function () {
+    const s = [
+      black, '##',
+      red, '##',
+      green, '##',
+      yellow, '##',
+      blue, '##',
+      magenta, '##',
+      cyan, '##',
+      white, '##',
+      gray, '##',
+      blackBright, '##',
+      redBright, '##',
+      greenBright, '##',
+      yellowBright, '##',
+      blueBright, '##',
+      magentaBright, '##',
+      cyanBright, '##',
+      whiteBright, '##',
+      reset,
+      resetAll
+    ].join('')
+    console.log(s)
+    assert.strictEqual(s, '\u001b[30m##\u001b[31m##\u001b[32m##\u001b[33m##\u001b[34m##\u001b[35m##\u001b[36m##\u001b[37m##\u001b[90m##\u001b[90m##\u001b[91m##\u001b[92m##\u001b[93m##\u001b[94m##\u001b[95m##\u001b[96m##\u001b[97m##\u001b[39m\u001b[0m')
+  })
+
+  it('shall display bg colors', function () {
+    const s = [
+      white,
+      bgBlack, '##',
+      bgRed, '##',
+      bgGreen, '##',
+      bgYellow, '##',
+      bgBlue, '##',
+      bgMagenta, '##',
+      bgCyan, '##',
+      black,
+      bgWhite, '##',
+      white,
+      bgBlackBright, '##',
+      bgRedBright, '##',
+      black,
+      bgGreenBright, '##',
+      bgYellowBright, '##',
+      bgBlueBright, '##',
+      bgMagentaBright, '##',
+      bgCyanBright, '##',
+      bgWhiteBright, '##',
+      bgReset
+    ].join('')
+    console.log(s)
+    assert.strictEqual(s, '\u001b[37m\u001b[40m##\u001b[41m##\u001b[42m##\u001b[43m##\u001b[44m##\u001b[45m##\u001b[46m##\u001b[30m\u001b[47m##\u001b[37m\u001b[100m##\u001b[101m##\u001b[30m\u001b[102m##\u001b[103m##\u001b[104m##\u001b[105m##\u001b[106m##\u001b[107m##\u001b[49m')
+  })
+
+  it('shall expand bg', function () {
+    const s = [
+      bgCyanBright,
+      bgExpand,
+      'ABCDEF',
+      bgReset
+    ].join('')
+    console.log(s)
+    assert.strictEqual(s, '\u001b[106m\u001b[KABCDEF\u001b[49m')
+  })
+})
