0770f8346b38cd3b93b4a69ac116b38d8ccda6dd
commenthol <commenthol@gmail.com>
2021-11-05 08:35:35 +0200

    fix(array/flatten): use ES2019 .flat()

--- src/array/flatten.js
+++ src/array/flatten.js
@@ -3,4 +3,7 @@
  * @example
  * flatten([1, [2], [[3], 4], 5]) //> [1, 2, 3, 4, 5]
  */
-export const flatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? flatten(v) : v)))
+// export const flatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? flatten(v) : v)))
+
+// using ES2019 .flat()
+export const flatten = arr => arr.flat(Infinity)
