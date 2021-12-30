cb094e4316c65b9b357e51403a372df2369d14cf
commenthol <commenthol@gmail.com>
2021-12-30 10:23:43 +0100

    fix(set): do not overwrite arrays

--- src/object/set.js
+++ src/object/set.js
@@ -1,17 +1,30 @@
 /**
  * set `value` at `keys` from `object`
+ * @param {object} obj
+ * @param {string|string[]} keys
+ * @param {any} value
+ * @returns {object}
  * @example
  * set({a: {b: {c: 2}}}, ['a', 'b', 'c'], 3)
+ * //> {a: {b: {c: 3}}}
  */
 export const set = (obj, keys = [], value) => {
   let key
   let ref
-  let o = obj
+  let tmp = obj
   for (key of keys) {
-    ref = o
-    if (toString.call(o[key]) !== '[object Object]') o[key] = {}
-    o = o[key]
+    ref = tmp
+    const type = Object.prototype.toString.call(tmp[key])
+    if (type !== '[object Object]' && type !== '[object Array]') {
+      tmp[key] = {}
+    }
+    tmp = tmp[key]
   }
-  ref[key] = value
+
+  if (Array.isArray(ref[key])) {
+    ref[key].push(value)
+  } else {
+    ref[key] = value
+  }
   return obj
 }
--- src/object/set.spec.js
+++ src/object/set.spec.js
@@ -6,8 +6,14 @@ describe('object/set', () => {
     const obj = {}
     assert.deepStrictEqual(set(obj, ['a', 'b', 'c'], 3), { a: { b: { c: 3 } } })
   })
-  it('should overwrite `a.b`', () => {
+
+  it('should append array at `a.b`', () => {
     const obj = { a: { b: [1, 2, 3] } }
-    assert.deepStrictEqual(set(obj, ['a', 'b'], 3), { a: { b: 3 } })
+    assert.deepStrictEqual(set(obj, ['a', 'b'], 4), { a: { b: [1, 2, 3, 4] } })
+  })
+
+  it('should append array `a.b` with object', () => {
+    const obj = { a: { b: [1, 2, 3] } }
+    assert.deepStrictEqual(set(obj, ['a', 'b'], { c: 3 }), { a: { b: [1, 2, 3, { c: 3 }] } })
   })
 })
