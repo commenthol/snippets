b48be4926d6df5c6229e5e53e3cc5898dfef51eb
commenthol <commenthol@gmail.com>
2021-12-30 10:01:52 +0100

    feat(string): caseConversions

---
+++ src/string/caseConversions.js
@@ -0,0 +1,42 @@
+/**
+ * capitalize the first letter
+ * @param {string} str
+ * @returns {string}
+ */
+export const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1)
+
+/**
+ * Provides conversion functions which works reliable in both ways
+ */
+
+/**
+ * convert lowerCamelCase to snake_case
+ * @param {string} str
+ * @returns {string}
+ */
+export const camelToSnakeCase = (str) => str
+  .replace(/([A-Z])/g, (_, m) => `_${m.toLowerCase()}`)
+
+/**
+ * convert snake_case to lowerCamelCase
+ * @param {string} str
+ * @returns {string}
+ */
+export const snakeToCamelCase = (str) => str.toLowerCase()
+  .replace(/_([a-z])/g, (_, m) => m.toUpperCase())
+
+/**
+ * convert lowerCamelCase to kebab-case
+ * @param {string} str
+ * @returns {string}
+ */
+export const camelToKebabCase = (str) => str
+  .replace(/([A-Z])/g, (_, m) => `-${m.toLowerCase()}`)
+
+/**
+ * convert kebab-case to lowerCamelCase
+ * @param {string} str
+ * @returns {string}
+ */
+export const kebabToCamelCase = (str) => str.toLowerCase()
+  .replace(/[-_]\w/g, (m) => m[1].toUpperCase())
---
+++ src/string/caseConversions.spec.js
@@ -0,0 +1,42 @@
+import assert from 'assert'
+import {
+  capitalizeFirstLetter,
+  camelToSnakeCase,
+  snakeToCamelCase,
+  camelToKebabCase,
+  kebabToCamelCase
+} from './index.js'
+
+describe('string/caseConversions', () => {
+  it('capitalize first letter', () => {
+    assert.strictEqual(capitalizeFirstLetter('lowercase'), 'Lowercase')
+  })
+
+  it('should convert lowerCamelCase to snake case', () => {
+    assert.strictEqual(camelToSnakeCase('lowerCamelCase'), 'lower_camel_case')
+  })
+
+  it('should convert lowerCamelCase to snake case', () => {
+    assert.strictEqual(camelToSnakeCase('IAmWritingXMLAndHTML'), '_i_am_writing_x_m_l_and_h_t_m_l')
+  })
+
+  it('should convert UpperCamelCase to snake case', () => {
+    assert.strictEqual(camelToSnakeCase('UpperCamelCase'), '_upper_camel_case')
+  })
+
+  it('should convert from snake case', () => {
+    assert.strictEqual(snakeToCamelCase('lower_camel_case'), 'lowerCamelCase')
+  })
+
+  it('should convert lowerCamelCase to kamel case', () => {
+    assert.strictEqual(camelToKebabCase('lowerCamelCase'), 'lower-camel-case')
+  })
+
+  it('should convert from kebab case', () => {
+    assert.strictEqual(kebabToCamelCase('lower-camel-case'), 'lowerCamelCase')
+  })
+
+  it('should convert from falsy kebab case', () => {
+    assert.strictEqual(kebabToCamelCase('lower-CaMel-case'), 'lowerCamelCase')
+  })
+})
