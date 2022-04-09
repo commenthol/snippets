e2f2e8d9e7357ef2d71df1f979da0a1660e44969
commenthol <commenthol@gmail.com>
2022-04-09 10:28:29 +0200

    feat(node): runall for package.json scripts

---
+++ src/node/runall.mjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+/**
+ * `npm run` with multiple arguments
+ * can be used as a simple replacement for [npm-run-all](https://npmjs.org/package/npm-run-all)
+ * usage in package.json
+ * ```
+ * "scripts": { "ci": "node ./runall.mjs lint build test", ... }
+ * ```
+ */
+
+import { spawn } from 'child_process'
+
+function npmRun (arg) {
+  return new Promise((resolve, reject) => {
+    const sub = spawn('npm', ['run', arg], { stdio: 'inherit' })
+    sub.on('error', err => { reject(err) })
+    sub.on('exit', code => {
+      code > 0
+        ? reject(new Error('' + code))
+        : resolve()
+    })
+  })
+}
+
+async function main (argv) {
+  for (const arg of argv) {
+    await npmRun(arg)
+  }
+}
+
+main(process.argv.slice(2))
+  .catch((err) => {
+    console.error(err.message)
+    process.exit(1)
+  })
