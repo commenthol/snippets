dad9c873b115ef81a13d3f96f070cc53bb2a5f5b
commenthol <commenthol@gmail.com>
2022-05-29 07:28:07 +0200

    fix(node/http): fetch uses URL parser

--- src/node/http/fetch.js
+++ src/node/http/fetch.js
@@ -1,40 +1,33 @@
-/* eslint node/no-deprecated-api: warn */
-
-import { parse } from 'url'
 import http from 'http'
 import https from 'https'
 
-export function fetch (url, { ...options } = {}) {
-  const opts = { ...parse(url), method: 'GET', headers: { 'User-Agent': 'fetch/1.0' }, ...options }
-  const transport = opts.protocol === 'https:' ? https : http
-  const req = transport.request(opts)
-
-  const self = {}
-
-  self.then = (_resolve) => new Promise((resolve, reject) => {
-    req.once('response', res => {
-      if (res.statusCode >= 300 && res.statusCode <= 400) {
-        console.error(res.statusCode, res.headers.location) // eslint-disable-line
-        reject(new Error('redirect not supported'))
-        return
-      }
-      let text = ''
-      res.text = async () => text
-      res.json = async () => JSON.parse(text)
-      res.on('data', (data) => {
-        text += data.toString()
+export function fetch (url, opts) {
+  const { protocol, hostname, port, pathname, search } = new URL(url)
+  const { body, ..._opts } = opts || {}
+  const path = pathname + search
+  const transport = protocol === 'http:' ? http : https
+  const req = transport.request({ ..._opts, hostname, port, path })
+  const then = (_resolve) =>
+    new Promise((resolve, reject) => {
+      req.once('response', res => {
+        let text = ''
+        res.status = res.statusCode
+        res.ok = res.status < 300
+        res.headers = Object.entries(res.headers)
+        res.text = async () => text
+        res.json = async () => JSON.parse(text)
+        res.on('data', (data) => { text += data.toString() })
+        res.on('error', reject)
+        res.on('end', () => resolve(res))
+        req.on('error', reject)
       })
-      res.on('error', reject)
-      res.on('end', () => resolve(res))
-    })
-    req.on('error', reject)
-  }).then(_resolve)
+    }).then(_resolve)
+  req.end(body)
 
   // non standard
-  self.pipe = (stream) => {
+  function pipe (stream) {
     req.once('response', res => res.pipe(stream))
   }
 
-  req.end()
-  return self
+  return { then, pipe }
 }
