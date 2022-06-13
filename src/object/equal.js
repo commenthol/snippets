62fbb47d7dd434e4dcb0d0f4a6eb9c788f4b50a5
commenthol <commenthol@gmail.com>
2022-06-13 13:57:44 +0200

    fix(object): equal comparison

--- src/object/equal.js
+++ src/object/equal.js
@@ -1,1 +1,48 @@
-export const equal = (a, b) => JSON.stringify(a) === JSON.stringify(b)
+/**
+ * simple comparison for equality using JSON.stringify()
+ * problem: fails on cicular objects
+ * @param {any} a
+ * @param {any} b
+ * @returns {boolean}
+ */
+export const equalSimple = (a, b) => JSON.stringify(a) === JSON.stringify(b)
+
+/**
+ * compare for equality
+ * @param {any} a
+ * @param {any} b
+ * @returns {boolean} true if a equals b
+ */
+export const equal = (a, b, visited = { a: [], b: [] }) => {
+  if (a === b) {
+    return true
+  }
+
+  if (Object.prototype.toString.call(a) !== Object.prototype.toString.call(b)) {
+    return false
+  }
+
+  if (a instanceof Date && b instanceof Date) {
+    return a.getTime() === b.getTime()
+  }
+
+  if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) {
+    return a === b
+  }
+
+  if (a.prototype !== b.prototype) return false
+
+  const keys = Object.keys(a)
+  if (keys.length !== Object.keys(b).length) return false
+
+  if (~visited.a.indexOf(a) || ~visited.b.indexOf(b)) {
+    return ~visited.a.indexOf(a) === ~visited.b.indexOf(b)
+  }
+
+  visited.a.push(a)
+  visited.b.push(b)
+  const result = keys.every(k => equal(a[k], b[k], visited))
+  visited.a.pop()
+  visited.b.pop()
+  return result
+}
--- src/object/equal.spec.js
+++ src/object/equal.spec.js
@@ -1,16 +1,71 @@
 import assert from 'assert'
-import { equal } from './index.js'
+import { equal, equalSimple } from './index.js'
 
 describe('object/equal', () => {
-  it('should be equal', () => {
-    assert.strictEqual(
-      equal({}, {}), true
-    )
+  describe('equalSimple', function () {
+    it('should be equal', () => {
+      assert.strictEqual(
+        equalSimple({}, {}), true
+      )
+    })
+
+    it('should not be equal', () => {
+      assert.strictEqual(
+        equalSimple({}, null), false
+      )
+    })
   })
 
-  it('should not be equal', () => {
-    assert.strictEqual(
-      equal({}, null), false
-    )
+  describe('equal', function () {
+    it('should be equal', () => {
+      assert.strictEqual(equal({}, {}), true)
+      assert.strictEqual(equal(), true)
+      assert.strictEqual(equal(5, 5), true)
+      assert.strictEqual(equal('A', 'A'), true)
+      assert.strictEqual(equal(new Date(123), new Date(123)), true)
+    })
+
+    it('should no be equal', () => {
+      assert.strictEqual(equal({}, null), false)
+      assert.strictEqual(equal(5, '5'), false)
+      assert.strictEqual(equal(5, '6'), false)
+      assert.strictEqual(equal('', '6'), false)
+      assert.strictEqual(equal(6, ''), false)
+      assert.strictEqual(equal(undefined, '6'), false)
+      assert.strictEqual(equal(new Date(123), new Date(124)), false)
+    })
+
+    it('shall compare deep equal', function () {
+      assert.strictEqual(equal(
+        { a: [2, { e: 3 }], b: [4], c: 'foo' },
+        { a: [2, { e: 3 }], b: [4], c: 'foo' }
+      ), true)
+    })
+
+    it('shall fail on wrong type', function () {
+      assert.strictEqual(equal(
+        [1, 2, 3], { 0: 1, 1: 2, 2: 3 }
+      ), false)
+    })
+
+    it('shall pass on same circularities', function () {
+      const a = { b: { bb: 1 }, c: { cc: 2 } }
+      a.b.c = a.b
+
+      const b = { b: { bb: 1 }, c: { cc: 2 } }
+      b.b.c = b.b
+
+      assert.strictEqual(equal(a, b), true)
+    })
+
+    it('shall fail on different circularities', function () {
+      const a = { b: { bb: 1 }, c: { cc: 2 } }
+      a.b.c = a.b
+
+      const b = { b: { bb: 1 }, c: { cc: 2 } }
+      b.c.c = b.b
+
+      assert.strictEqual(equal(a, b), false)
+    })
   })
 })
