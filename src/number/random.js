0a094eb40cf96955f35334a0a983926c3d189821
commenthol <commenthol@gmail.com>
2022-06-25 09:42:34 +0200

    fix(number/random): split float from integer

--- src/number/random.js
+++ src/number/random.js
@@ -2,22 +2,22 @@
  * create a random number
  * @param {number} lower
  * @param {number} upper
- * @param {boolean} useFloat
  * @returns {number}
  */
-export function random (lower = 0, upper = 1, useFloat) {
-  if (useFloat === undefined) {
-    if (lower === true) {
-      useFloat = true
-      upper = 1
-      lower = 0
-    } else if (upper === true) {
-      useFloat = true
-      upper = lower
-      lower = 0
-    }
-    useFloat = useFloat || !!(upper % 1 || lower % 1)
-  }
-  const rand = lower + (upper - lower) * Math.random()
-  return useFloat ? rand : Math.floor(rand + 0.5)
+export function random (lower = 1, upper = 0) {
+  const _lower = Math.min(lower, upper)
+  const _upper = Math.max(lower, upper)
+  return _lower + (_upper - _lower) * Math.random()
+}
+
+/**
+ * create a random number integer
+ * @param {number} lower
+ * @param {number} upper
+ * @returns {number}
+ */
+export function randomInt (lower = 1, upper = 0) {
+  const _lower = Math.min(lower, upper)
+  const _upper = Math.max(lower, upper)
+  return Math.floor(_lower + (1 + _upper - _lower) * Math.random())
 }
--- src/number/random.spec.js
+++ src/number/random.spec.js
@@ -1,5 +1,5 @@
 import assert from 'assert'
-import { random } from './index.js'
+import { random, randomInt } from './index.js'
 
 const assertRange = (result, expLower, expUpper, isInteger) => {
   assert(result >= expLower, 'lower bound assertion')
@@ -13,38 +13,36 @@ const assertRange = (result, expLower, expUpper, isInteger) => {
 
 describe('number/random', function () {
   it('no args', function () {
-    assertRange(random(), 0, 1, true)
+    assertRange(random(), 0, 1)
   })
 
-  it('upper range', function () {
-    assertRange(random(5), 0, 5, true)
-  })
-
-  it('lower & upper range', function () {
-    assertRange(random(10, 50), 10, 50, true)
-  })
-
-  it('float: no args', function () {
-    assertRange(random(true), 0, 1)
-  })
-
-  it('float: no args', function () {
-    assertRange(random(true), 0, 1)
-  })
-
-  it('float: upper range float', function () {
+  it('upper range float', function () {
     assertRange(random(1.2), 0, 1.2)
   })
 
-  it('float: upper range int', function () {
-    assertRange(random(2, true), 0, 2)
+  it('upper range int', function () {
+    assertRange(random(2), 0, 2)
   })
 
-  it('float: lower & upper range float', function () {
+  it('lower & upper range float', function () {
     assertRange(random(5, 12.2), 5, 12.2)
   })
 
-  it('float: lower & upper range int', function () {
-    assertRange(random(2, 10, true), 2, 10)
+  it('lower & upper range int', function () {
+    assertRange(random(2, 10), 2, 10)
+  })
+})
+
+describe('number/randomInt', function () {
+  it('no args', function () {
+    assertRange(randomInt(), 0, 1, true)
+  })
+
+  it('upper range', function () {
+    assertRange(randomInt(5), 0, 5, true)
+  })
+
+  it('lower & upper range', function () {
+    assertRange(randomInt(10, 50), 10, 50, true)
   })
 })
