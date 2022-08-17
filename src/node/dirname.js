8228c0427aeaf0e05c12ef8334c49e06ce2a837c
commenthol <commenthol@gmail.com>
2022-08-17 19:08:59 +0200

    fix(node): dirname usong path.dirname

--- src/node/dirname.js
+++ src/node/dirname.js
@@ -2,14 +2,15 @@
  * construction of __filename and __dirname in es module
  */
 
+import path from 'path'
 import { fileURLToPath } from 'url'
 
-export const getDirname = (meta) => fileURLToPath(new URL('.', meta)).replace(/\/$/, '')
+export const getDirname = (meta) => path.dirname(fileURLToPath(meta))
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
+if (process.argv[1] === __filename) {
   // eslint-disable-next-line no-console
   console.log({ __filename, __dirname })
 }
