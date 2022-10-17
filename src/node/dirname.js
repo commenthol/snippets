01d9f53ba79b046b1e81230fe65b408beb62ce1e
commenthol <commenthol@gmail.com>
2022-10-17 20:11:50 +0200

    fix(node/dirname): load files with new ULR()

--- src/node/dirname.js
+++ src/node/dirname.js
@@ -3,6 +3,7 @@
  */
 
 import path from 'path'
+import fs from 'fs'
 import { fileURLToPath } from 'url'
 
 export const getDirname = (meta) => path.dirname(fileURLToPath(meta))
@@ -10,7 +11,19 @@ export const getDirname = (meta) => path.dirname(fileURLToPath(meta))
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+/**
+ * replacement for `require.main === module`
+ */
 if (process.argv[1] === __filename) {
   // eslint-disable-next-line no-console
   console.log({ __filename, __dirname })
 }
+
+// get parent path uri
+console.log(new URL('..', import.meta.url).href)
+
+// get relative file uri
+console.log(new URL('../index.js', import.meta.url).href)
+
+// loading files
+console.log(fs.readFileSync(new URL('../index.js', import.meta.url), 'UTF-8'))
