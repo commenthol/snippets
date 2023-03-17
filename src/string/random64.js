4f07bbcda19f57ce3d85096bab26c5654641780c
commenthol <commenthol@gmail.com>
2023-03-17 21:05:13 +0100

    feat(string/random64): option to not use an dashes in random string

--- src/string/random64.js
+++ src/string/random64.js
@@ -6,14 +6,18 @@ import { webcrypto as crypto } from 'node:crypto'
  * with length = 21 same entropy as with uuid can be achieved
  * (random64) 64^21 > (uuid4) 16^31
  * @param {number} [length=21] of string
+ * @param {boolean} [noDashes] only use chars [0-9a-zA-Z]
  * @returns {string}
  */
-export const random64 = (length = 21) => {
+export const random64 = (length = 21, noDashes = false) => {
   const rand = crypto.getRandomValues(new Uint8Array(length))
   let str = ''
+  const getByte = noDashes
+    ? (i) => rand[i] % 61
+    : (i) => rand[i] & 63
   for (let i = 0; i < length; i += 1) {
-    let byte = rand[i]
-    str += (byte &= 63) < 36
+    const byte = getByte(i)
+    str += byte < 36
       ? byte.toString(36) // 0-9 a-z
       : byte < 62
         ? (byte - 26).toString(36).toUpperCase() // A-Z
--- src/string/random64.spec.js
+++ src/string/random64.spec.js
@@ -2,6 +2,7 @@ import assert from 'assert'
 import { random64 } from './random64.js'
 
 const RE = /^[0-9a-zA-Z_-]+$/
+const RE_NO_DASHES = /^[0-9a-zA-Z]+$/
 
 describe('string/random64', function () {
   it('shall generate a random Id', function () {
@@ -14,5 +15,11 @@ describe('string/random64', function () {
     const id = random64(40)
     assert.strictEqual(id.length, 40)
     assert(RE.test(id))
+  })
+
+  it('shall generate a random Id with length 40 and no dashes', function () {
+    const id = random64(40, true)
+    assert.strictEqual(id.length, 40)
+    assert(RE_NO_DASHES.test(id))
   })
 })
