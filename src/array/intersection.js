8e3c4330301dd9e4caca9c437b4bf73743102b68
commenthol <commenthol@gmail.com>
2023-03-25 17:57:27 +0200

    feat(array): intersection

---
+++ src/array/intersection.js
@@ -0,0 +1,7 @@
+/**
+ * @param {any[]} arr
+ * @param {any[]} comp
+ * @returns {any[]} overlapping values
+ */
+export const intersection = (arr, comp) =>
+  arr.filter(item => comp.some((el) => el === item))
