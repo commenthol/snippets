6185470ad8b88a0e158fb41c91dd2dc38aad84df
commenthol <commenthol@gmail.com>
2023-06-17 10:12:23 +0200

    feat: Storybook for VanJS

---
+++ src/vanjs/stories/Storybook.js
@@ -0,0 +1,129 @@
+import styles from './Storybook.module.css'
+import van from 'van-dom'
+const {
+  a,
+  aside,
+  div,
+  h4,
+  main,
+  p,
+  section
+} = van.tags
+
+const classnames = (...args) => (args.filter(Boolean).join(' '))
+
+/**
+ * @typedef {object} Story
+ * @property {string} title
+ * @property {() => HTMLElement} component
+ */
+
+/**
+ * Simple Storybook for vanjs
+ * @param {object} props
+ * @param {Story[] | HTMLElement[] | Function[]} props.stories stories
+ * @param {string} [props.header='Storybook'] title
+ * @param {string} [props.href='/'] header link
+ * @returns {HTMLElement}
+ */
+export default function Storybook (props) {
+  const {
+    stories,
+    header = 'Storybook',
+    href = '/stories/index.html'
+  } = props
+
+  const Component = van.state(() =>
+    p(
+      'The simple storybook for ',
+      a({
+        href: 'https://github.com/vanjs-org/van',
+        target: '_blanc',
+        rel: 'norel noreferrer'
+      },
+      'vanjs'
+      )
+    )
+  )
+
+  // state change causes full rerendering...
+  const active = van.state('')
+  const locHash = decodeURIComponent(location.hash.substring(1))
+
+  const setActiveComponent = (component, title) => {
+    active.val = title
+    Component.val = component
+  }
+
+  const handleClick = (component, title) => (ev) => {
+    setActiveComponent(component, title)
+  }
+
+  return main(
+    { className: classnames(styles.storybook) },
+    van.bind(active, (active) => aside(
+      { className: classnames(styles.stories) },
+      h4(
+        { onclick: () => { location.href = href } },
+        header
+      ),
+      stories.map((component, index) =>
+        Story({
+          component,
+          index,
+          locHash,
+          handleClick,
+          active,
+          setActiveComponent
+        })
+      )
+    )),
+    van.bind(Component,
+      Component => section(
+        { className: 'stories' },
+        Component()
+      )
+    )
+  )
+}
+
+function Story (props) {
+  const { component, index, handleClick, active, setActiveComponent, locHash } = props
+
+  let title
+  let _component
+
+  if (component instanceof HTMLElement) {
+    return component
+  } else if (component.title) {
+    title = component.title
+    _component = component.component
+  } else {
+    return null
+  }
+
+  if (!active && !locHash && index === 0) {
+    window.requestAnimationFrame(() => {
+      setActiveComponent(_component, title)
+    })
+  } else if (!active && title === locHash) {
+    window.requestAnimationFrame(() => {
+      setActiveComponent(_component, title)
+    })
+  }
+
+  const className = title === active ? styles.active : ''
+
+  return div(
+    a(
+      {
+        href: `#${title}`,
+        tabIndex: 0,
+        'aria-role': 'button',
+        className,
+        onclick: handleClick(_component, title)
+      },
+      title
+    )
+  )
+}
---
+++ src/vanjs/stories/main.js
@@ -0,0 +1,40 @@
+import Storybook from './Storybook'
+import van from 'van-dom'
+const {
+  button,
+  div,
+  small
+} = van.tags
+
+// -- define or import your stories ---
+
+const storyButton = {
+  title: 'Button',
+  component: () => button(
+    { onclick: () => alert('Hi') },
+    'Click me'
+  )
+}
+
+const Counter = () => {
+  const counter = van.state(0)
+  return div(
+    button({ onclick: () => --counter.val }, '-'),
+    counter,
+    button({ onclick: () => ++counter.val }, '+')
+  )
+}
+const storyCounter = {
+  title: Counter.name,
+  component: Counter
+}
+
+// --- add them to the storybook ---
+
+van.add(document.body, Storybook({
+  stories: [
+    small('Components'),
+    storyButton,
+    storyCounter
+  ]
+}))
