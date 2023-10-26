becddfae5d8369d0a0771bfab6d47ed5175e02f3
commenthol <commenthol@gmail.com>
2023-10-26 20:45:01 +0200

    fix(crypto/base64Uint8): simplify

--- src/crypto/base64Uint8.js
+++ src/crypto/base64Uint8.js
@@ -2,17 +2,23 @@
  * @param {Uint8Array} uint8
  * @returns {string} base64 encoded string
  */
-export const uint8ToBase64 = (uint8) => {
-  const out = []
-  for (const n of uint8) {
-    out.push(String.fromCharCode(n))
-  }
-  return btoa(out.join(''))
-}
+export const uint8ToBase64 = (uint8) => btoa(String.fromCodePoint(...uint8))
 
 /**
  * @param {string} b64Encoded base64 encoded string
  * @returns {Uint8Array}
  */
 export const base64ToUint8 = (b64Encoded) =>
-  Uint8Array.from(atob(b64Encoded).split('').map(c => c.charCodeAt(0)))
+  Uint8Array.from(atob(b64Encoded), (c) => c.codePointAt(0))
+
+/**
+ * @param {string} str utf16 or javascript string
+ * @returns {string} base64 encoded string
+ */
+export const toBase64 = (str) => uint8ToBase64(new TextEncoder().encode(str))
+
+/**
+ * @param {string} str utf16 or javascript string
+ * @returns {string} base64 encoded string
+ */
+export const fromBase64 = (b64Encoded) => new TextDecoder().decode(base64ToUint8(b64Encoded))
--- src/crypto/base64Uint8.spec.js
+++ src/crypto/base64Uint8.spec.js
@@ -1,9 +1,19 @@
 import assert from 'node:assert/strict'
-import { uint8ToBase64, base64ToUint8 } from './base64Uint8.js'
+import {
+  uint8ToBase64,
+  base64ToUint8,
+  toBase64,
+  fromBase64
+} from './base64Uint8.js'
 
 describe('crypto/base64Uint8', function () {
-  const str = 'abc ABC Text \x00 👏🏽 🇺🇳 👩🏼‍🦱'
-  const arr = Uint8Array.from(unescape(encodeURIComponent(str)).split('').map(c => c.charCodeAt(0)))
+  const str = 'abc ABC Text \x00 👏🏽 🇺🇳 👩🏼‍🦱 🥨'
+  const b64 = Buffer.from(str).toString('base64')
+  const arr = Uint8Array.from(
+    unescape(encodeURIComponent(str))
+      .split('')
+      .map((c) => c.charCodeAt(0))
+  )
 
   it('shall base64 encode Uint8Array', function () {
     const b64Encoded = uint8ToBase64(arr)
@@ -13,5 +23,13 @@ describe('crypto/base64Uint8', function () {
   it('shall decode base64 string to Uint8Array', function () {
     const decoded = base64ToUint8(Buffer.from(str).toString('base64'))
     assert.deepEqual(decoded, arr)
+  })
+
+  it('shall encode string to base64 encoded utf8 string', function () {
+    assert.equal(toBase64(str), b64)
+  })
+
+  it('shall decode base64 encoded utf8 string', function () {
+    assert.equal(fromBase64(b64), str)
   })
 })
