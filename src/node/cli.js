4b40dacd1c0db9298c714bd506d241b26ae822b3
commenthol <commenthol@gmail.com>
2023-10-26 20:46:43 +0200

    feat(node/cli): improve cli commander

--- src/node/cli.js
+++ src/node/cli.js
@@ -1,28 +1,50 @@
 /**
+ * @typedef {object} CliOption
+ * @property {string} [short] short option
+ * @property {string} [long] long option
+ * @property {'number'|'string'} [type] type of option
+ * @property {number|string} [def] default option
+ * @property {string} [help]
+ */
+
+/**
  * a cli commander
- * @type {Object}
+ *
  * @example
  * const cmmds = {
- *   //     short, long, args, helptext, default value
- *   help: ['-h', '--help', false, 'this help'],
- *   test: ['-t', '--test', 'string', 'test', 'this is a test'],
+ *   help: { short: '-h', long: '--help', help: 'this help' },
+ *   test: { short: '-t', long: '--test', type: 'string', def: 'test', help: 'this is a test' },
  * }
- * cli(cmmds, []) // { test: 'this is a test', _hasArgs: false, _helptext: () => '' }
- * cli(cmmds, ['--test', 'hi world']) // { test: 'hi world', _hasArgs: true, _helptext: () => '' }
-*/
+ * cli(cmmds, [])
+ * // { test: 'this is a test', hasArgs: false, helptext: '\n    Usage: myprg [options]\n\n...' }
+ * cli(cmmds, ['--test', 'hi world'])
+ * // { test: 'hi world', hasArgs: true, helptext: '\n    Usage: myprg [options]\n\n...' }
+ *
+ * @param {Record<string, CliOption>} cmmds
+ * @param {string[]|[]} argv
+ * @returns {Record<string, boolean|number|string>}
+ */
 export function cli (cmmds, argv = process.argv.slice(2)) {
   const cmd = { helptext: '\n    Usage: myprg [options]\n\n' }
+
   const map = Object.entries(cmmds).reduce((o, [key, vals]) => {
-    const [short, long, shift, help, def] = vals
-    cmd.helptext += `    ${String(short).padEnd(2)}, ${String(long).padEnd(10)} ${(shift || '').padEnd(10)} ${help}\n`
-    if (def) cmd[key] = def
+    const { short = '', long = '', type, help = '', def } = vals
+    cmd.helptext += `    ${String(short).padEnd(2)}, ${String(long).padEnd(
+      10
+    )} ${(type || '').padEnd(10)} ${help}\n`
+    if (def !== undefined) {
+      cmd[key] = def
+    }
     o[short] = o[long] = () => {
-      cmd[key] = shift ? argv.shift() : true
+      const val = type ? nextArg(_argv) ?? def ?? true : true
+      cmd[key] = type === 'number' ? Number(val) : val
     }
     return o
   }, {})
-  while (argv.length) {
-    const arg = argv.shift()
+
+  const _argv = expand(argv)
+  while (_argv.length) {
+    const arg = _argv.shift()
     const found = map[arg]
     if (found) {
       cmd.hasArgs = true
@@ -32,4 +54,27 @@ export function cli (cmmds, argv = process.argv.slice(2)) {
     }
   }
   return cmd
+}
+
+function expand (argv) {
+  const nArgv = []
+  for (const arg of argv) {
+    if (/^-[a-z]+$/.test(arg)) {
+      const shortArgs = arg.slice(1).split('')
+      for (const short of shortArgs) {
+        nArgv.push(`-${short}`)
+      }
+    } else {
+      nArgv.push(arg)
+    }
+  }
+  return nArgv
+}
+
+function nextArg (argv) {
+  const next = argv[0]
+  if (typeof next !== 'string' || next.startsWith('-')) {
+    return
+  }
+  return argv.shift()
 }
--- src/node/cli.spec.js
+++ src/node/cli.spec.js
@@ -3,31 +3,80 @@ import { cli } from './cli.js'
 
 describe('node/cli', function () {
   const cmmds = {
-    help: ['-h', '--help', false, 'this help'],
-    version: ['-v', '--version', false, 'display version'],
-    todo: ['-t', '--todo', 'string', 'add todo']
+    help: { short: '-h', long: '--help', help: 'this help' },
+    version: { short: '-v', long: '--version', help: 'display version' },
+    todo: { short: '-t', long: '--todo', type: 'string', help: 'add todo' },
+    a: { long: '--rm', help: 'remove all' }
   }
+
+  const helptext =
+    '\n    Usage: myprg [options]\n\n    -h, --help                this help\n    -v, --version             display version\n    -t, --todo     string     add todo\n      , --rm                  remove all\n'
+
   it('shall parse arguments', function () {
-    const cmd = cli(cmmds, '--help --version --todo todo file1 file2'.split(/ /))
+    const cmd = cli(
+      cmmds,
+      '--help --version --todo todo file1 file2'.split(/ /)
+    )
     assert.deepStrictEqual(cmd, {
       args: ['file1', 'file2'],
       help: true,
       todo: 'todo',
       version: true,
       hasArgs: true,
-      helptext: '\n    Usage: myprg [options]\n\n    -h, --help                this help\n    -v, --version             display version\n    -t, --todo     string     add todo\n'
+      helptext
     })
     // console.log(JSON.stringify(_helptext()))
   })
   it('shall parse short arguments', function () {
-    const { _helptext, ...cmd } = cli(cmmds, '-h file1 -v file2 -t todo'.split(/ /))
+    const cmd = cli(
+      cmmds,
+      '-h file1 -v file2 -t todo'.split(/ /)
+    )
     assert.deepStrictEqual(cmd, {
       args: ['file1', 'file2'],
       help: true,
       todo: 'todo',
       version: true,
       hasArgs: true,
-      helptext: '\n    Usage: myprg [options]\n\n    -h, --help                this help\n    -v, --version             display version\n    -t, --todo     string     add todo\n'
+      helptext
+    })
+  })
+
+  it('shall expand short arguments', function () {
+    const cmd = cli(
+      cmmds,
+      '-hvt todo file1 file2'.split(/ /)
+    )
+    assert.deepStrictEqual(cmd, {
+      args: ['file1', 'file2'],
+      help: true,
+      todo: 'todo',
+      version: true,
+      hasArgs: true,
+      helptext
+    })
+  })
+
+  it('shall ignore missing option', function () {
+    const cmd = cli(cmmds, '-t -v'.split(/ /))
+    assert.deepStrictEqual(cmd, {
+      todo: true,
+      version: true,
+      hasArgs: true,
+      helptext
+    })
+  })
+
+  it('shall replace missing option with default', function () {
+    const _cmmds = { ...cmmds, a: { short: '-a', type: 'number', def: 42 } }
+    const cmd = cli(_cmmds, '-tva'.split(/ /))
+    assert.deepStrictEqual(cmd, {
+      todo: true,
+      version: true,
+      a: 42,
+      hasArgs: true,
+      helptext:
+        '\n    Usage: myprg [options]\n\n    -h, --help                this help\n    -v, --version             display version\n    -t, --todo     string     add todo\n    -a,            number     \n'
     })
   })
 })
