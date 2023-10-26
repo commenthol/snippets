97a8f95f825050438edde2cdc5efd51682a88ea8
commenthol <commenthol@gmail.com>
2023-10-26 20:47:04 +0200

    fix(node/testing): run simple mocha standalone

--- src/node/testing/mocha.spec.js
+++ src/node/testing/mocha.spec.js
@@ -3,42 +3,44 @@ import { describe as describeX, it } from './mocha.js'
 
 const sleep = (ms = 10) => new Promise((resolve) => setTimeout(() => resolve(), ms))
 
-describeX('simple mocha', function () {
-  it('ok synch', function () {
-    assert.ok(true)
-  })
-
-  describeX('good cases', function () {
-    it('ok callback', function (done) {
-      assert.ok(true)
-      done()
-    })
-
-    it('ok promise', async function () {
-      await sleep(1)
+if (!global.describe) {
+  describeX('simple mocha', function () {
+    it('ok synch', function () {
       assert.ok(true)
     })
-  })
 
-  describeX('bad cases', function () {
-    it('fail synch', function () {
-      assert.ok(false)
+    describeX('good cases', function () {
+      it('ok callback', function (done) {
+        assert.ok(true)
+        done()
+      })
+
+      it('ok promise', async function () {
+        await sleep(1)
+        assert.ok(true)
+      })
     })
 
-    it('fail callback', function (done) {
-      assert.ok(false, 'callback fails')
-      done()
+    describeX('bad cases', function () {
+      it('fail synch', function () {
+        assert.ok(false)
+      })
+
+      it('fail callback', function (done) {
+        assert.ok(false, 'callback fails')
+        done()
+      })
+
+      it('fail promise', async function () {
+        await sleep(1)
+        assert.ok(false)
+      })
+
+      it.skip('skip me', function () {})
     })
 
-    it('fail promise', async function () {
-      await sleep(1)
-      assert.ok(false)
+    describeX.skip('skip cases', function () {
+      it('avoid', function () {})
     })
-
-    it.skip('skip me', function () {})
   })
-
-  describeX.skip('skip cases', function () {
-    it('avoid', function () {})
-  })
-})
+}
