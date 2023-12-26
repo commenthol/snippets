03d72b1ce70d747f52f66dab9b229fdae2654058
commenthol <commenthol@gmail.com>
2023-12-26 10:25:20 +0100

    feat(function): signal

---
+++ src/function/signal.js
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @see https://dev.to/ryansolid/building-a-reactive-library-from-scratch-1i0p
+ * @license (c) 2022 Ryan Carniato
+ * License is assumed to be MIT or similar as with solid-js
+ */
+
+/**
+ * @typedef {{
+ *  execute: () => void
+ *  dependencies: Set<Dependency>
+ * }} Subscriber
+ */
+/**
+ * @typedef {Set<Subscriber>} Dependency
+ */
+
+// global context for nested reactivity
+const context = []
+
+/**
+ * @param {any} value
+ * @returns {[signal: () => any, set: (nextValue: any) => void]}
+ */
+export function createSignal (value) {
+  /** @type {Set<Subscriber>} */
+  const subscriptions = new Set()
+
+  const signal = () => {
+    const running = context[context.length - 1]
+    if (running) {
+      subscriptions.add(running)
+      running.dependencies.add(subscriptions)
+      // console.debug(value, subscriptions.size)
+    }
+    return value
+  }
+
+  const set = (nextValue) => {
+    value = nextValue
+
+    for (const running of [...subscriptions]) {
+      // emit events
+      running.execute()
+    }
+  }
+  return [signal, set]
+}
+
+/**
+ * @param {Subscriber} running
+ */
+function cleanup (running) {
+  for (const dep of running.dependencies) {
+    dep.delete(running)
+  }
+  running.dependencies.clear()
+}
+
+/**
+ * @param {() => void} fn
+ */
+export function createEffect (fn) {
+  const execute = () => {
+    cleanup(running)
+    context.push(running)
+    try {
+      fn()
+    } finally {
+      context.pop()
+    }
+  }
+
+  const running = {
+    execute,
+    dependencies: new Set()
+  }
+
+  execute()
+}
+
+/**
+ * @param {()=> void} fn
+ * @returns {() => void} signal
+ */
+export function createMemo (fn) {
+  const [signal, set] = createSignal()
+  createEffect(() => set(fn()))
+  return signal
+}
---
+++ src/function/signal.spec.js
@@ -0,0 +1,92 @@
+import assert from 'node:assert'
+import { createSignal, createEffect, createMemo } from './signal.js'
+
+describe('function/signal', function () {
+  it('shall update signal', function () {
+    const arr = []
+    const [count, setCount] = createSignal(3)
+
+    arr.push(count())
+    setCount(5)
+    arr.push(count())
+    setCount(count() * 2)
+    arr.push(count())
+    assert.deepEqual(arr, [3, 5, 10])
+  })
+
+  it('with effect', function () {
+    const arr = []
+    // 1. Create Signal
+    const [count, setCount] = createSignal(0)
+
+    // 2. Create Reaction
+    createEffect(() => arr.push(count()))
+    // 3. Set count to 5
+    setCount(5)
+    // 4. Set count to 10
+    setCount(10)
+
+    assert.deepEqual(arr, [0, 5, 10])
+  })
+
+  it('conditional rendering', function () {
+    const arr = []
+
+    // 1. Create'
+    const [firstName] = createSignal('John')
+    const [lastName, setLastName] = createSignal('Smith')
+    const [showFullName, setShowFullName] = createSignal(true)
+
+    const displayName = createMemo(() => {
+      if (!showFullName()) return firstName()
+      return `${firstName()} ${lastName()}`
+    })
+
+    createEffect(() => arr.push(displayName()))
+
+    // 2. Set showFullName: false
+    setShowFullName(false)
+    // 3. Change lastName
+    setLastName('Legend')
+    // 4. Set showFullName: true
+    setShowFullName(true)
+
+    assert.deepEqual(arr, ['John Smith', 'John', 'John Legend'])
+  })
+
+  it('nested effects pile up subscriptions', function () {
+    const arr = []
+    const [firstName, setFirstName] = createSignal('John')
+    const [lastName, setLastName] = createSignal('Smith')
+
+    createEffect(() => {
+      // console.debug(`firstName=${firstName()}`)
+      arr.push(firstName())
+
+      // NOTE: creates a new subscription on every update
+      createEffect(() => {
+        // console.debug(`lastName=${lastName()}`)
+        arr.push(lastName())
+      })
+    })
+
+    setFirstName('Joe')
+    setFirstName('Jack')
+    setLastName('Legend')
+
+    assert.equal(firstName(), 'Jack')
+    assert.equal(lastName(), 'Legend')
+
+    assert.deepEqual(arr, [
+      'John',
+      'Smith',
+      'Joe',
+      'Smith',
+      'Jack',
+      'Smith',
+      'Legend',
+      'Legend',
+      'Legend'
+    ])
+  })
+})
