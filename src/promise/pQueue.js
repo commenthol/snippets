febdfacfa367710bdf886c5c2b23d3eed5a2a67e
commenthol <commenthol@gmail.com>
2023-12-27 14:44:20 +0100

    feat(promise/pQueue): an async queue

---
+++ src/promise/pQueue.js
@@ -0,0 +1,120 @@
+/**
+ * @returns {{
+ *  promise: Promise
+ *  resolve: (any) => {}
+ * }}
+ */
+const promised = () => {
+  const p = {}
+  p.promise = new Promise((resolve) => {
+    p.resolve = resolve
+  })
+  return p
+}
+
+/**
+ * @typedef {() => Promise<any>} AsyncFunction
+ */
+/**
+ * @typedef {object} PQueueOptions
+ * @property {number} [limit=10]
+ * @property {number} [timeout=20] timeout in ms
+ */
+/**
+ * @typedef {object} PQueue
+ * @property {AsyncFunction[]} add
+ * @property {() => number} size
+ * @property {Promise<PromiseSettledResult[]>} onEmpty
+ */
+
+/**
+ * an async queue with concurrency and timeout
+ * @param {PQueueOptions} param
+ * @returns {PQueue}
+ * @example
+ * // concurrency = 3; timeout until resolving = 50ms
+ * const queue = pQueue({ limit: 3, timeout: 50 })
+ * queue.add(async() => 5)
+ * const result = await queue.onEmpty()
+ * //> [{status: 'fulfilled', value: 5}]
+ */
+export const pQueue = (param) => {
+  const { limit = 10, timeout = 20 } = param || {}
+  const result = []
+  const p = promised()
+  const fns = []
+  let i = 0
+  let running = 0
+  let isResolved = false
+  let timerId
+
+  function retrigger () {
+    clearTimeout(timerId)
+    timerId = setTimeout(() => {
+      if (!isResolved && running <= 0) {
+        isResolved = true
+        p.resolve(result)
+      }
+    }, timeout)
+  }
+
+  function runner () {
+    const c = i
+    const fn = fns.shift()
+    const done = fn === undefined
+    const promise = (typeof fn === 'function')
+      ? fn()
+      : (!done) && Promise.reject(new TypeError('no function'))
+
+    if (done) {
+      running--
+    } else {
+      i++
+    }
+    if (!isResolved && running <= 0) {
+      retrigger()
+    } else if (promise) {
+      promise
+        .then(value => { result[c] = { status: 'fulfilled', value } })
+        .catch(err => { result[c] = { status: 'rejected', reason: err } })
+        .finally(() => { runner() })
+    }
+  }
+
+  /**
+   * Add task(s) to queue
+   * @param  {AsyncFunction[]} tasks
+   */
+  function add (...tasks) {
+    if (isResolved) {
+      throw new Error('pQueue has been resolved')
+    }
+    if (!tasks.length) return
+    tasks.forEach(fn => fns.push(fn))
+    const l = Math.min(fns.length, limit)
+    while (running < l) {
+      running++
+      setImmediate(() => { runner() })
+    }
+  }
+
+  function onEmpty () {
+    if (!isResolved && running <= 0) {
+      retrigger()
+    }
+    return p.promise
+  }
+
+  /**
+   * @returns {number} size of queue
+   */
+  function size () {
+    return fns.length
+  }
+
+  return {
+    add,
+    size,
+    onEmpty
+  }
+}
---
+++ src/promise/pQueue.spec.js
@@ -0,0 +1,62 @@
+import { deepEqual } from 'assert'
+import { pQueue } from './pQueue.js'
+
+const nap = (ms = 10) => new Promise(resolve => setTimeout(() => resolve(ms), ms))
+
+const promised = () => {
+  const p = {}
+  p.promise = new Promise((resolve) => {
+    p.resolve = resolve
+  })
+  return p
+}
+
+const fulfilled = result => result.map(({ status, value, reason }) => {
+  if (status === 'rejected') {
+    return { status, reason: reason?.message }
+  }
+  return { status, value }
+})
+
+describe('promise/pQueue', () => {
+  it('shall resolve if empty', async function () {
+    const queue = pQueue()
+    const result = await queue.onEmpty()
+    deepEqual(result, [])
+  })
+
+  it('shall process queued items', async function () {
+    const queue = pQueue()
+    queue.add(() => Promise.resolve('resolve'))
+    queue.add(() => nap(20))
+    queue.add(() => Promise.reject(new Error('reject')))
+    queue.add(() => nap(0))
+
+    const result = await queue.onEmpty()
+    deepEqual(fulfilled(result), [
+      { status: 'fulfilled', value: 'resolve' },
+      { status: 'fulfilled', value: 20 },
+      { status: 'rejected', reason: 'reject' },
+      { status: 'fulfilled', value: 0 }
+    ])
+  })
+
+  it('should wait till timeout for adding task', async function () {
+    const p = promised()
+    const queue = pQueue({ timeout: 10 })
+    queue.onEmpty().then(result => {
+      p.resolve(result)
+    })
+    await nap(9)
+    queue.add(() => nap(6))
+    queue.add(() => nap(8))
+    await nap(9)
+    queue.add(() => nap(10))
+    const result = await p.promise
+    deepEqual(result, [
+      { status: 'fulfilled', value: 6 },
+      { status: 'fulfilled', value: 8 },
+      { status: 'fulfilled', value: 10 }
+    ])
+  })
+})
