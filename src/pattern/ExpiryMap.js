2c161420d249cc716b6c203d22394c350004c202
commenthol <commenthol@gmail.com>
2024-02-08 21:59:54 +0100

    feat(pattern/ExpiryMap): Map with expiring entries

---
+++ src/pattern/ExpiryMap.js
@@ -0,0 +1,64 @@
+/**
+ * map with expiring entries
+ */
+export class ExpiryMap extends Map {
+  /**
+   * @param {number} expires - in milliseconds; default 1min
+   * @param {number} interval - in milliseconds; default expires/2; 0 means that no
+   * cleanup timer is triggered
+   */
+  constructor (expires = 60e3, interval) {
+    super()
+    this._expires = expires
+    this._interval = interval ?? Math.max(100, expires / 2)
+    this._timer()
+  }
+
+  /**
+   * @private
+   */
+  _timer () {
+    if (this._interval <= 0 || this._timerId) {
+      return
+    }
+    this._timerId = setTimeout(() => {
+      this.cleanup()
+      this._timerId = undefined
+      this._timer()
+    }, this._interval)
+  }
+
+  /**
+   * cleanup expired entries
+   */
+  cleanup () {
+    for (const [key, { expiresAt }] of this.entries()) {
+      if (expiresAt <= Date.now()) {
+        this.delete(key)
+      }
+    }
+  }
+
+  get size () {
+    this.cleanup()
+    return super.size
+  }
+
+  get (key) {
+    const { expiresAt, value } = super.get(key) || {}
+    if (expiresAt > Date.now()) {
+      return value
+    }
+    this.delete(key)
+  }
+
+  /**
+   * @param {any} key
+   * @param {any} value
+   * @param {number} [expires] - expiry in milliseconds
+   */
+  set (key, value, expires = this._expires) {
+    const expiresAt = Date.now() + expires
+    return super.set(key, { value, expiresAt })
+  }
+}
---
+++ src/pattern/ExpiryMap.spec.js
@@ -0,0 +1,36 @@
+import assert from 'node:assert'
+import { ExpiryMap } from './index.js'
+import { sleep } from '../promise/index.js'
+
+describe('pattern/ExpiryMap', function () {
+  it('shall expire values', async function () {
+    const ec = new ExpiryMap(50)
+    ec.set('a', 1)
+    await sleep(40)
+    assert.equal(ec.get('a'), 1)
+    console.log(ec)
+    await sleep(15)
+    assert.equal(ec.get('a'), undefined)
+  })
+
+  it('shall set', async function () {
+    const ec = new ExpiryMap(50)
+    for (let i = 0; i < 100; i++) {
+      ec.set(i, i, 100 - i)
+    }
+    assert.ok(deviate(ec.size, 99), ec.size)
+    await sleep(30)
+    assert.ok(deviate(ec.size, 68), ec.size)
+    await sleep(30)
+    const found = []
+    for (let i = 0; i < 100; i++) {
+      const val = ec.get(i)
+      if (val !== undefined) found.push(val)
+    }
+    // console.log(found)
+    assert.ok(deviate(found.length, 37), found.length)
+  })
+})
+
+const deviate = (actual, expected, deviation = 2) =>
+  actual >= expected - deviation && actual <= expected + deviation
