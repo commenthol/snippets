673e5d6c8b3b95cc5116b0d71fe89c8983f91746
commenthol <commenthol@gmail.com>
2024-02-08 21:59:21 +0100

    fix(promise/sleep): return ms

--- src/promise/sleep.js
+++ src/promise/sleep.js
@@ -1,5 +1,6 @@
 /**
  * @param {number} ms sleep timeout in milliseconds
- * @returns {Promise<void>}
+ * @returns {Promise<number>}
  */
-export const sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms))
+export const sleep = (ms) =>
+  new Promise((resolve) => setTimeout(() => resolve(ms), ms))
