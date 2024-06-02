322013d0699e8e83d523ce4da61cad4378666b82
commenthol <commenthol@gmail.com>
2024-06-02 20:56:34 +0200

    feat(string/LocalURL): local urls without hostname or protocol

---
+++ src/string/localUrl.js
@@ -0,0 +1,46 @@
+export const PROTO = 'local://local'
+
+/**
+ * creates a URL representation which can deal with local url paths which do not
+ * contain a hostname or protocol
+ */
+export class LocalURL extends URL {
+  /**
+   * @param {string|URL} uri
+   */
+  constructor (uri = '') {
+    super(uri, PROTO)
+  }
+
+  /**
+   * @returns {string}
+   */
+  toString () {
+    const u = super.toString()
+    if (u.startsWith(PROTO)) {
+      return u.slice(PROTO.length)
+    }
+    return u
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  isLocal () {
+    return this.href.startsWith(PROTO)
+  }
+
+  /**
+   * set search params from object
+   * @param {Record<string,any>|{}} [obj]
+   * @returns {this}
+   */
+  setSearchParams (obj = {}) {
+    this.search = ''
+    for (const [name, value] of Object.entries(obj)) {
+      if (value === undefined || value === '') continue
+      this.searchParams.set(name, '' + value)
+    }
+    return this
+  }
+}
---
+++ src/string/localUrl.test.js
@@ -0,0 +1,53 @@
+import assert from 'assert'
+import { LocalURL } from './localUrl.js'
+
+describe('string/LocalURL', function () {
+  it('shall instantiate', function () {
+    const lurl = new LocalURL()
+    assert.equal(lurl.isLocal(), true)
+    assert.equal(lurl.toString(), '')
+  })
+
+  it('shall instantiate with pathname', function () {
+    const uri = '/path/one'
+    const lurl = new LocalURL(uri)
+    assert.equal(lurl.isLocal(), true)
+    assert.equal(lurl.toString(), uri)
+  })
+
+  it('shall instantiate with search params', function () {
+    const uri = '?foo=bar&test=1'
+    const lurl = new LocalURL(uri)
+    assert.equal(lurl.isLocal(), true)
+    assert.equal(lurl.toString(), uri)
+  })
+
+  it('shall instantiate with LocalURL', function () {
+    const uri = '/path/one?foo=bar&test=1'
+    const lurl1 = new LocalURL(uri)
+    const lurl = new LocalURL(lurl1)
+    assert.equal(lurl.isLocal(), true)
+    assert.equal(lurl, uri)
+  })
+
+  it('shall instantiate with URL', function () {
+    const url = new URL('https://test.tld/path/one?foo=bar&test=1')
+    const lurl = new LocalURL(url)
+    assert.equal(lurl.isLocal(), false)
+    assert.equal(lurl.toString(), url.toString())
+  })
+
+  it('shall instantiate with URLstring', function () {
+    const url = 'https://test.tld/path/one?foo=bar&test=1'
+    const lurl = new LocalURL(url)
+    assert.equal(lurl.isLocal(), false)
+    assert.equal(lurl.toString(), url)
+  })
+
+  it('shall reset search params', function () {
+    const url = 'https://test.tld/path/one?foo=bar&test=1'
+    const lurl = new LocalURL(url)
+    lurl.setSearchParams({ wat: 'baz', taste: 'yummy' })
+    assert.equal(lurl.toString(), 'https://test.tld/path/one?wat=baz&taste=yummy')
+  })
+})
