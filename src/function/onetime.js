037c97def6fdf1bbec8063fcdbd6126d1440d533
commenthol <commenthol@gmail.com>
2024-10-14 21:59:49 +0200

    feat(function): onetime

---
+++ src/function/onetime.js
@@ -0,0 +1,23 @@
+/**
+ * ensures that `fn` is only called once
+ * @template T
+ * @param {() => T} fn
+ * @returns {() => T}
+ */
+export function onetime (fn) {
+  let result
+  let isCalled = false
+
+  if (typeof fn !== 'function') {
+    throw new Error('fn needs to be a function')
+  }
+
+  return () => {
+    if (isCalled) {
+      return result
+    }
+    isCalled = true
+    result = fn()
+    return result
+  }
+}
---
+++ src/function/onetime.test.js
@@ -0,0 +1,12 @@
+import assert from 'assert'
+import { onetime } from './onetime.js'
+
+describe('function/onetime', () => {
+  it('shall only be called once', () => {
+    let i = 0
+    const one = onetime(() => ++i)
+    assert.equal(one(), 1)
+    assert.equal(one(), 1)
+    assert.equal(one(), 1)
+  })
+})
