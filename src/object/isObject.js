008be5036d1e8647c46511b83d424961d5d0519f
commenthol <commenthol@gmail.com>
2025-07-02 08:09:06 +0200

    fix(object): isObject typedef

--- src/object/isObject.js
+++ src/object/isObject.js
@@ -1,7 +1,7 @@
 /**
  * check if the given value is an object
  * @param {any} any
- * @returns {boolean}
+ * @returns {boolean is object}
  */
 export const isObject = (any) =>
   typeof any === 'object' && any !== null && Array.isArray(any) === false
