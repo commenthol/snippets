53034a90a0815326f9c2042c8a37f01cc4c19d31
commenthol <commenthol@gmail.com>
2025-07-02 08:08:43 +0200

    feat(object): merge objects and arrays

--- src/object/merge.js
+++ src/object/merge.js
@@ -1,18 +1,62 @@
+import { isObject } from './isObject.js'
+
 /**
  * merge objects
  * does not break on circularities
- * @param  {...Object} objs
+ * @param  {...Object} sources
  * @return {Object}
  */
-export const merge = (...objs) =>
-  [...objs].reduce((acc, obj) => {
-    if (!obj || typeof obj !== 'object') return obj
-    Object.keys(obj).forEach((k) => {
+export const mergeObj = (...sources) =>
+  [...sources].reduce((acc, source) => {
+    if (!source || typeof source !== 'object') return source
+    Object.keys(source).forEach((k) => {
       acc[k] =
         Object.prototype.hasOwnProperty.call(acc, k) &&
         typeof acc[k] === 'object'
-          ? merge(acc[k], obj[k])
-          : obj[k]
+          ? mergeObj(acc[k], source[k])
+          : source[k]
     })
     return acc
   }, {})
+
+/**
+ * merge objects and arrays
+ * does NOT do circular checks!
+ * @template T
+ * @param {Array<Partial<T> | undefined>} sources
+ * @returns {T}
+ */
+export const merge = (...sources) => {
+  /** @type {any} */
+  let target = {}
+
+  for (const source of sources) {
+    if (Array.isArray(source)) {
+      if (!Array.isArray(target)) {
+        target = []
+      }
+      target = [...target, ...source]
+    } else if (isObject(source)) {
+      // @ts-expect-error
+      for (let [key, value] of Object.entries(source)) {
+        if (isObject(value) && !propertyIsUnsafe(target, key)) {
+          value = merge(target[key], value)
+        }
+        target = { ...target, [key]: value }
+      }
+    }
+  }
+
+  return target
+}
+
+/**
+ * @private
+ * @param {any} target
+ * @param {string} key
+ * @returns {boolean}
+ */
+const propertyIsUnsafe = (target, key) =>
+  key in target &&
+  (!Object.hasOwnProperty.call(target, key) || // unsafe if they exist up the prototype chain,
+    !Object.propertyIsEnumerable.call(target, key))
--- src/object/merge.test.js
+++ src/object/merge.test.js
@@ -1,10 +1,55 @@
 import assert from 'assert'
-import { merge } from './index.js'
+import { mergeObj, merge } from './index.js'
 
-describe('object/merge', () => {
+describe('object/mergeObj', () => {
   it('should merge', () => {
     const obj = { a: { b: { c: 1 } } }
     const target = {}
+    assert.deepStrictEqual(mergeObj(target, obj), obj)
+  })
+  it('should merge 2 objects', () => {
+    const target = { a: { b: 3 } }
+    const obj1 = { a: { b: { c: 1 } } }
+    const obj2 = { a: { d: { e: 2 } } }
+    const exp = { a: { b: { c: 1 }, d: { e: 2 } } }
+    const res = mergeObj(target, obj1, obj2)
+    assert.deepStrictEqual(res, exp)
+  })
+  it('should merge other 2 objects', () => {
+    const target = { a: { b: { c: 1 } } }
+    const obj1 = { a: { b: 3 } }
+    const obj2 = { a: { d: { e: 2 } } }
+    const exp = { a: { b: 3, d: { e: 2 } } }
+    const res = mergeObj(target, obj1, obj2)
+    assert.deepStrictEqual(res, exp)
+  })
+  it('should merge object with arrays', () => {
+    const target = { a: { b: [{ c: 1 }] } }
+    const obj1 = { a: { b: [{ c: 2, d: 2 }, { e: 2 }] } }
+    const obj2 = { a: [1, 2, 3] }
+    const exp = { a: { 0: 1, 1: 2, 2: 3, b: { 0: { c: 2, d: 2 }, 1: { e: 2 } } } }
+    const res = mergeObj(target, obj1, obj2)
+    assert.deepStrictEqual(res, exp)
+  })
+  it('should merge base obj', () => {
+    const target = { a: 1 }
+    const obj1 = { a: 2 }
+    const exp = { a: 2 }
+    const res = mergeObj(target, obj1)
+    assert.deepStrictEqual(res, exp)
+  })
+  it('should be save against prototype pollution', function () {
+    const payload = '{"__proto__":{"oops":"It works !"}}'
+    const a = {}
+    mergeObj({}, JSON.parse(payload))
+    assert.strictEqual(a.oops, undefined)
+  })
+})
+
+describe('object/merge', () => {
+  it('should merge', () => {
+    const target = {}
+    const obj = { a: { b: { c: 1 } } }
     assert.deepStrictEqual(merge(target, obj), obj)
   })
   it('should merge 2 objects', () => {
@@ -27,7 +72,7 @@ describe('object/merge', () => {
     const target = { a: { b: [{ c: 1 }] } }
     const obj1 = { a: { b: [{ c: 2, d: 2 }, { e: 2 }] } }
     const obj2 = { a: [1, 2, 3] }
-    const exp = { a: { 0: 1, 1: 2, 2: 3, b: { 0: { c: 2, d: 2 }, 1: { e: 2 } } } }
+    const exp = { a: [1, 2, 3] }
     const res = merge(target, obj1, obj2)
     assert.deepStrictEqual(res, exp)
   })
