1aacbaee036e2563fc9fc95e6b57640b06fbe6f8
commenthol <commenthol@gmail.com>
2026-02-22 19:55:16 +0100

    fix(string): randomHex

--- src/string/randomHex.js
+++ src/string/randomHex.js
@@ -4,11 +4,13 @@
  * @example
  * randomHex() //> c165a012bdf67bd1
  */
-export const randomHex = (length = 16) =>
-  Array(length)
-    .fill(1)
-    .map(() => Math.random().toString(16)[3] || '0')
-    .join('')
+export const randomHex = (length = 16) => {
+  let str = ''
+  while (str.length < length) {
+    str += Math.random().toString(16).slice(2)
+  }
+  return str.substring(0, length)
+}
 
 /**
  * faster alternative which returns 13 byte long random hex string
